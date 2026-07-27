b5a92ddcf0bd99d3039f4ebb59125cc69b20c38c
Michael Kramer <michael.kramer.guitar@gmail.com>
2026-07-27 17:50:49 -0400

    feat(iter2) wrote a decent state machine

--- DAT/state/StateExt.py
+++ DAT/state/StateExt.py
@@ -12,25 +12,57 @@ from TDStoreTools import StorageManager
 import TDFunctions as TDF
 
 class StateExt:
-	"""
-	STATE is the temporal memory of the artwork.
+    """
+    STATE is the temporal memory of the artwork.
 
-	AUDIO describes what is happening now.
+    AUDIO describes what is happening now.
 
-	STATE describes what continues to exist after the sound has passed.
-        It does not process signals.
+    STATE describes what continues to exist after the sound has passed.
+    It does not process signals.
 
-        It accumulates experience.
-	"""
+    It accumulates experience.
+    """
 
     def __init__(self, ownerComp):
-
         self.ownerComp = ownerComp
-
         self._state = {}
 
-    def Set(self, key, value):
-        self._state[key] = value
+    def Set(self, key: str, value: str, source=None) -> None:
+        """
+        key: state name
+        value: state value
+        source: Caller Component
+        """
+        self._state[key] = {
+            "value": value,
+            "source": source
+        }
+        print(f"StateExt.Set(): {key}: {value} - source component: {source}")
+        print("StateExt.Set(): calling RefreshDashboard()")
+        self.RefreshDashboard()
 
     def Get(self, key, default=None):
         return self._state.get(key, default)
+
+    def Remove(self, key):
+        print(f"StateExt.Remove(): attempting to remove key '{key}'")
+        self._state.pop(key, None)
+        self.RefreshDashboard()
+
+
+    def RefreshDashboard(self):
+        table = self.ownerComp.op("state_table")
+
+        table.clear()
+
+        print("StateExt.RefreshDashboard(): clearing table...")
+        table.appendRow(["Feature", "Value", "Source"])
+
+        for key, entry in self._state.items():
+            print(f"StateExt.RefreshDashboard(): adding {key}: {entry['value']} " 
+                  f"for source component {entry['source']}")
+            table.appendRow([
+                key, 
+                entry["value"], 
+                entry["source"]
+            ])
